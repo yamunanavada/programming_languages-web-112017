9b6c30d7e3e592c0595dfa286f1e50f00fdf24d3
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 17:50:01 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,3 @@
-require "pry"
 def reformat_languages(languages)
   # your code here
 
@@ -6,8 +5,17 @@ def reformat_languages(languages)
 
   languages.each do |style, lang|
     languages[style].each do |language, type|
-      new_hash[language] = {}
-
+      new_hash[language] = type
+      new_hash[language][:style] = []
+    end
+  end
+  
+  languages.each do |style, lang|
+    languages[style].each do |lange, type|
+      puts lange
+      if new_hash.include? lange
+        new_hash[lange][:style] << style
+      end
     end
   end
 new_hash
