c52632cdc55c6a5d81b6505aacf7c46da92ecb9c
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 17:30:35 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+require "pry"
 def reformat_languages(languages)
   # your code here
+
+  new_hash = {}
+
+  languages.each do |style, language|
+    new_hash[language] = {}
+    binding.pry
+  end
+
+  
 end
