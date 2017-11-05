2dd578a6eac125064730174441f648b2d05f1ddc
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 17:50:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
 def reformat_languages(languages)
   # your code here
+
+  new_hash = {}
+
+  languages.each do |style, lang|
+    languages[style].each do |language, type|
+      new_hash[language] = type
+      new_hash[language][:style] = []
+    end
+  end
+
+  languages.each do |style, lang|
+    languages[style].each do |lange, type|
+      puts lange
+      if new_hash.include? lange
+        new_hash[lange][:style] << style
+      end
+    end
+  end
+new_hash
+
 end
